12868424f5f54572a20bc3fcd0e871790de5e54a
Alexander Stoisolovich <alexander.stoisolovich@gmail.com>
2022-10-01 10:49:12 -0400

    Added tests for a sum function

--- test/test.spec.js
+++ test/test.spec.js
@@ -1,5 +1,5 @@
 const { expect } = require('chai');
-const { minimum, maximum, mean } = require('../source/descriptive');
+const { minimum, maximum, mean, sum } = require('../source/descriptive');
 
 describe('Descriptive statistics function', () => {
 
@@ -38,6 +38,17 @@ describe('Descriptive statistics function', () => {
     it('returns the value five', () => {
       let result = mean(...values);
       expect(result).to.equal(5);
+    });
+  });
+
+  describe('sum function', () => {
+    it('returns a value of type number', () => {
+      let result = sum(...values);
+      expect(typeof result).to.equal('number');
+    });
+    it('returns the value fifty-five', () => {
+      let result = sum(...values);
+      expect(result).to.equal(55);
     });
   });
 
